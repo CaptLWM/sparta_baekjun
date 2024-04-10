93f9ea196e4c51f922145f8ebb6849409a487229
LWM <87697962+CaptLWM@users.noreply.github.com>
2024-04-10 15:12:49 +0900

    [Silver II] Title: 외판원 순회 2, Time: 516 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,18 +1,39 @@
-n = int(input())
-l = [list(map(int, input().split())) for _ in range(n)]
-visit = [0] * n
+import sys
+from itertools import *
+from collections import *
+
+# sys.stdin = open('input2.txt')
+
+# N : 도시의수
+n = int(sys.stdin.readline().strip())
+route_price = [list(map(int, input().split())) for _ in range(n)]
+visited = [False] * n
 m = 1e9
 
 def dfs(depth, start, cost):
     global m
-    if depth == n-1 and l[start][0] != 0:
-        m = min(m, cost+l[start][0])
+    if depth == n-1 and route_price[start][0] != 0:
+        m = min(m, cost+route_price[start][0])
         return
     for i in range(n):
-        if not visit[i] and l[start][i] != 0:
-            visit[i] = 1
-            dfs(depth+1, i, cost+l[start][i])
-            visit[i] = 0
-visit[0] = 1
+        if not visited[i] and route_price[start][i] != 0:
+            visited[i] = True
+            dfs(depth+1, i, cost+route_price[start][i])
+            visited[i] = False
+visited[0] = True
 dfs(0, 0, 0)
 print(m)
+
+# 범위 1000000 이므로 시간초과 발생
+# for i in cityboard:
+#     # print('check', cityboard[i])
+#     temp=list(i) +[i[0]] # 0에서 시작해서 0으로 돌아온다 가정
+#     temp_result=0
+#     for j in range(len(temp)-1):
+#         if route_Price[temp[j]][temp[j+1]] == 0:
+#             continue
+#         temp_result += route_Price[temp[j]][temp[j+1]]
+#     m = min(m, temp_result)
+#     # for j in range(len(cityboard[i])):
+#         # print('check',cityboard[i][j])
+#         # print('check2', i, j)
