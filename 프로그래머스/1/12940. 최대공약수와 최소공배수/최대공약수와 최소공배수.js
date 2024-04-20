3a7bc99d3adf44b3c3ca4f107a44bf4996635ea1
LWM <87697962+CaptLWM@users.noreply.github.com>
2024-04-20 21:51:59 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 2.91 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,6 +1,6 @@
 function solution(n, m) {
     var answer = [];
-    
+    // 2개의 자연수 a, b(a > b)에 대해서 a를 b로 나눈 나머지가 r일 때, a와 b의 최대공약수는 b와 r의 최대공약수와 같다
     function test(n,m) {
         let r=0
         while(m !=0) {
@@ -11,6 +11,7 @@ function solution(n, m) {
         return n
     }
     
+    // 두 수 a와 b의 최소공배수는 a와 b의 곱을 a와 b의 최대공약수를 나눈 것과 같다. 
     function test2(n, m) {
         return (n*m)/test(n,m)
     }
