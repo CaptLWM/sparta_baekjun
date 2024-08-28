6eb24e986e9c5637039fe6c77e79389d1044f17f
LWM <87697962+CaptLWM@users.noreply.github.com>
2024-08-28 20:29:14 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 8.22 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -0,0 +1,9 @@
+function solution(arr, queries) {
+    for(let i = 0 ; i < queries.length ; i++){
+        const[s,e,k] = queries[i];
+        for(let j = s ; j <= e ; j++)
+            if(j%k===0 ) arr[j]++;
+
+    }
+    return arr;
+}
