d8f24933103c0cfc312a407222848a7c6b1782ca
LWM <87697962+CaptLWM@users.noreply.github.com>
2024-10-12 16:13:17 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 24.40 ms, Memory: 38.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -0,0 +1,44 @@
+function solution(maps) {
+    // 맵 구조
+    const n = maps.length;
+    const m = maps[0].length;
+    
+    // 방향
+    const direction = [
+        [1,0], // 아래
+        [-1,0], // 위
+        [0,1], // 우
+        [0,-1] // 좌
+    ];
+    
+    const queue = [[0,0,1]]  // [x,y,거리]
+    
+    // 방문여부 체크
+    // const visited = Array.from({length:n},()=>Array(m).fill(false));
+    // 시작점
+    maps[0][0] = 0
+    
+    // 경로 탐색 시작
+    while (queue.length) {
+        // 이동 저장
+        const [x,y,distance] = queue.shift();
+        
+        // 목표위치 도달 시 거리 반환 
+        if(x===n-1 && y === m-1) {
+            return distance;
+        }
+        
+        // 상하좌우 탐색
+        for(const [dx, dy] of direction) {
+            const nx = x+dx;
+            const ny = y+dy;
+            
+            if(nx>=0 && nx<n && ny>=0 && ny<m && maps[nx][ny] === 1 ) {
+                maps[nx][ny] = 0;
+                queue.push([nx, ny, distance +1])
+            }
+        }
+        
+    }
+    return -1
+}
