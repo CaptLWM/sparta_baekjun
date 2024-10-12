64fac1f57472c13dd23e9137ca12145518599878
LWM <87697962+CaptLWM@users.noreply.github.com>
2024-10-12 16:36:08 +0900

    [level 2] Title: 하노이의 탑, Time: 4.82 ms, Memory: 48.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    var answer = [];
+    // num:현재 원판 수, from:시작기둥, to:목표기둥, aux:보조기둥
+    function hanoi(num, from, to, aux) {
+        if(num===1) {
+            answer.push([from, to])
+            return
+        }
+        hanoi(num-1, from, aux, to) // n-1개의 원판을 보조 기둥으로 이동
+        answer.push([from, to]); // N번째 원판을 목표 기둥으로 이동
+        hanoi(num-1, aux, to, from) // 보조기동에서 목표 기둥으로 N-1개의 원판 이동
+    }
+    hanoi(n, 1,3,2)
+    return answer;
+}
+
