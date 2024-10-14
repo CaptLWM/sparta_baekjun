be4009f94eaaac5a2aef7eea45f342555f6de2e2
LWM <87697962+CaptLWM@users.noreply.github.com>
2024-10-14 21:00:59 +0900

    [level 2] Title: 구명보트, Time: 37.88 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,17 @@
+function solution(people, limit) {
+    people.sort((a,b)=>a-b);
+    let left=0; // 가장 가까운 사람의 포인터
+    let right= people.length-1; // 가장 무거운 사람의 포인터
+    let boats=0; // 보트의수
+
+    while (left <= right) {
+        // 가장 가벼운 사람과 가장 무거운 사람의 합이 limit 이하인 경우
+        if(people[left]+people[right]<=limit) {
+            left++
+        }
+        right--;
+        boats++; // 보트의 수 증가
+    }
+    
+    return boats
+}
