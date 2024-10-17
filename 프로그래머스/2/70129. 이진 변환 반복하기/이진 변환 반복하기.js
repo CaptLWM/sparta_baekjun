b674a6d0077df6cbb45c72086e4a261911c27f9f
LWM <87697962+CaptLWM@users.noreply.github.com>
2024-10-17 12:10:53 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 11.16 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,13 +1,26 @@
 function solution(s) {
-  let countTransformations = 0; // 변환 횟수
-    let countZeros = 0; // 제거된 '0'의 개수
-
+    let answer = 0; // 변환 횟수
+    let counterZeros = 0; // 제거된 '0'의 개수
+    
+    // 최종 결과가 1이 될때까지 반복
     while (s !== '1') {
-        countTransformations++;
-        countZeros += (s.match(/0/g) || []).length; // '0' 개수 세기
-        s = s.replace(/0/g, ''); // '0' 제거
-        s = s.length.toString(2); // 길이를 이진수로 변환
+        // 횟수 카운트 시작 
+        answer++;
+        counterZeros += (s.match(/0/g) || []).length; // '0' 개수 세기
+        s = s.replace(/0/g, ''); // 0제거
+        s = s.length.toString(2) // 길이를 이진수로 변환
     }
+    
+    return [answer, counterZeros]
+//   let countTransformations = 0; // 변환 횟수
+//     let countZeros = 0; // 제거된 '0'의 개수
 
-    return [countTransformations, countZeros];
+//     while (s !== '1') {
+//         countTransformations++;
+//         countZeros += (s.match(/0/g) || []).length; // '0' 개수 세기
+//         s = s.replace(/0/g, ''); // '0' 제거
+//         s = s.length.toString(2); // 길이를 이진수로 변환
+//     }
+
+//     return [countTransformations, countZeros];
 }
