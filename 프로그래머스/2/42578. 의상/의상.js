095011f58ccb3956e82db9bc882325d4cb67b81e
LWM <87697962+CaptLWM@users.noreply.github.com>
2024-10-19 08:26:39 +0900

    [level 2] Title: 의상, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,19 +1,40 @@
 function solution(clothes) {
-    var answer = 0;
-    
+    let answer = 0;
     const categories = {};
-    clothes.forEach(([_,category])=> {
+    
+    clothes.forEach(([_, category])=>{
         if(!categories[category]) {
-            categories[category] = 0
+            categories[category]=0;
         }
-        categories[category]++
+        categories[category]++;
     })
     
-    console.log(categories)
     let combinations = 1;
     for(let count of Object.values(categories)) {
-        console.log(count)
-        combinations *=(count+1)
+        combinations *= (count+1)
     }
-    return combinations-1;
+    
+    return combinations -1
+//     var answer = 0;
+    
+//     const categories = {};
+    
+//     // 의상정보 카테고리 별로 구분
+//     clothes.forEach(([_,category])=>{
+//          if (!categories[category]) {
+//              categories[category] = 0;
+//          }
+//         categories[category]++;
+//     })
+//     // ex){ headgear: 2, eyewear: 1 }
+//     console.log(categories)
+    
+//     // 각 카테고리에서 선택할 수 있는 의상 수 + 1
+//     let combinations = 1;
+//     for (let count of Object.values(categories)) {
+//         combinations *= (count + 1)
+//     }
+    
+//     // 아무 의상도 선택하지 않는 경우를 제외
+//     return combinations - 1 
 }
