6c5c7e16662c74ac64868924c177417dcf1e9fbd
LWM <87697962+CaptLWM@users.noreply.github.com>
2024-10-18 20:51:58 +0900

    [level 3] Title: 입국심사, Time: 316.97 ms, Memory: 40.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -0,0 +1,26 @@
+function solution(n, times) {
+    // 최소 시간은 1초, 최대 시간은 가장 긴 처리시간 * 인원수
+    let left = 1;
+    let right = Math.max(...times) * n;
+    let answer = right;
+    
+    while(left<=right) {
+        const mid = Math.floor((left+right)/2);
+        let count = 0;
+        
+        // 각 심사대에세 mid 시간 내에 처리할 수 있는 인원 수 계산
+        for (let time of times) {
+            count += Math.floor(mid/time)
+        }
+        
+        // 처리할 수 있는 인원이 n명 이상이면 시간 조정
+        if (count >= n) {
+            answer= mid; // 가능한 최소 시간 업데이트
+            right = mid-1; // 최소 시간을 줄여봄
+        } else {
+            left = mid+1; // 시간을 늘려야함
+        }
+    }
+    
+    return answer
+}
